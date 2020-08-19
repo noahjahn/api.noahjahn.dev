1cd0eccc26be0c808221cf4c09e7988af140b120
Noah Jahn <najahn19@gmail.com>
2020-08-19 19:46:19 -0400

    Install and setup cors

--- middleware/index.js
+++ middleware/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const authentication = require('./authentication');
 const response = require('./response');
 const routes = require('../routes');
 
 module.exports = express()
+    .use(cors())
     .use(bodyParser.json())
     .use(bodyParser.urlencoded({extended:true}))
     .use(authentication)
