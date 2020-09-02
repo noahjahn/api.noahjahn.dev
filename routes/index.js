1045e7fff8879dd038f61136646314126d806487
Noah Jahn <najahn19@gmail.com>
2020-09-01 21:10:30 -0400

    Add default route to root of application

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ const passport = require('passport');
 const authentication = require('../components/authentication');
 const v1 = require('./v1');
 
+router.get('/', (req, res) => {
+    res.status(200).send();
+});
 router.use('/authentication', authentication);
 router.use('/v1', passport.authenticate('jwt', { session: false }), v1);
 
